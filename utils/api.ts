69b25709335b220060e9cdb9f4f5205cb5e86d10
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-06-25 20:07:34 +0300

    add api.ts

---
+++ utils/api.ts
@@ -0,0 +1,34 @@
+import { Product } from "@/types/product";
+
+const API_URL = "";
+
+export const fetchProducts = async (): Promise<Product[]> => {
+    const response = await fetch(API_URL);
+    return response.json();
+};
+
+export const addProduct = async (product: Partial<Product>): Promise<void> => {
+    await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(product),
+    });
+};
+
+export const updateProduct = async (id: number, product: Partial<Product>): Promise<void> => {
+    await fetch(`${API_URL}/${id}`, {
+        method: "PUT",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(product),
+    });
+};
+
+export const deleteProduct = async (id: number): Promise<void> => {
+    await fetch(`${API_URL}/${id}`, {
+        method: "DELETE",
+    });
+};
