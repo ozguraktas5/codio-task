5ae7c755f4d7076cc4d177ea8a1b25aaefeb9771
Özgür Aktaş <114170361+ozguraktas5@users.noreply.github.com>
2024-07-02 11:53:59 +0300

    added scss settings

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import useTranslation from 'next-translate/useTranslation';
+import useTranslation from "next-translate/useTranslation";
 import { Product } from "@/types/product";
 import styles from "../styles/ProductList.module.scss";
 import { fetchProducts, deleteProduct } from "../utils/api";
@@ -9,7 +9,7 @@ import LanguageSwitcher from "@/components/LanguageSwitcher";
 import ThemeSwitcher from "@/components/ThemeSwitcher";
 
 const ProductList: React.FC = () => {
-  const { t } = useTranslation('common');
+  const { t } = useTranslation("common");
   const [products, setProducts] = useState<Product[]>([]);
   const [sortCriteria, setSortCriteria] = useState<string>("default");
   const router = useRouter();
@@ -54,47 +54,66 @@ const ProductList: React.FC = () => {
   };
 
   return (
-    <div>
-      <h1>{t('title')}</h1>
-      <ThemeSwitcher />
-      <LanguageSwitcher />
-      <SortProduct
-        sortCriteria={sortCriteria}
-        setSortCriteria={setSortCriteria}
-      />
+    <div className={styles.all}>
+      <h1>{t("title")}</h1>
+      <div className={styles.settings}>
+        <ThemeSwitcher />
+        <LanguageSwitcher />
+        <SortProduct
+          sortCriteria={sortCriteria}
+          setSortCriteria={setSortCriteria}
+        />
+      </div>
+
       <div className={styles.productList}>
         {products.map((product) => (
           <div key={product.id} className={styles.productItem}>
             {product.imageUrl && (
-              <img src={product.imageUrl} alt={product.name} className={styles.productImage} />
+              <img
+                src={product.imageUrl}
+                alt={product.name}
+                className={styles.productImage}
+              />
             )}
             <h3>{product.name}</h3>
-            <p>{t('brand')}: {product.brand}</p>
-            <p>{t('model')}: {product.model}</p>
-            <p>{t('color')}: {product.color}</p>
-            <p>{t('price')}: ${product.price}</p>
-            <p>{t('popularity')}: {product.popularity}</p>
-            <p>
-              {t('created_at')}:
+            <div>
+              {t("brand")}: {product.brand}
+            </div>
+            <div>
+              {t("model")}: {product.model}
+            </div>
+            <div>
+              {t("color")}: {product.color}
+            </div>
+            <div>
+              {t("price")}: ${product.price}
+            </div>
+            <div>
+              {t("popularity")}: {product.popularity}
+            </div>
+            <div>
+              {t("created_at")}:
               {new Date(product.createdAt).toLocaleDateString("tr-TR", {
                 day: "2-digit",
                 month: "2-digit",
                 year: "numeric",
               })}
-            </p>
+            </div>
 
-            <button
-              className={styles.editButton}
-              onClick={() => handleEdit(product.id)}
-            >
-              {t('edit')}
-            </button>
-            <button
-              className={styles.deleteButton}
-              onClick={() => handleDelete(product.id)}
-            >
-              {t('delete')}
-            </button>
+            <div className={styles.buttons}>
+              <button
+                className={styles.editButton}
+                onClick={() => handleEdit(product.id)}
+              >
+                {t("edit")}
+              </button>
+              <button
+                className={styles.deleteButton}
+                onClick={() => handleDelete(product.id)}
+              >
+                {t("delete")}
+              </button>
+            </div>
           </div>
         ))}
       </div>
